3aca9f6ddd15f09d182bdbc7be72898e79864615
arekiu <alex.isaguilera@hotmail.com>
2024-04-18 16:43:09 +0200

    add isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aschmidt <aschmidt@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/18 12:51:11 by aschmidt          #+#    #+#             */
-/*   Updated: 2024/04/18 12:51:13 by aschmidt         ###   ########.fr       */
+/*   Created: 2024/04/18 16:29:47 by aschmidt          #+#    #+#             */
+/*   Updated: 2024/04/18 16:29:51 by aschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*#include <stdio.h>
 
-int	ft_isalpha(int numchar);
+int	ft_isdigit(int numchar);
 
 int	main(void)
 {
-	printf("%d", ft_isalpha('Z'));
+	printf("%d", ft_isdigit('d'));
 }
 */
-int	ft_isalpha(int num)
+int	ft_isdigit(int num)
 {
-	if ((num >= 'a' && num <= 'z') || (num >= 'A' && num <= 'Z'))
+	if (num >= '0' && num <= '9')
 		return (1);
 	else
 		return (0);
