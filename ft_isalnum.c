0722df68bb9203434b01477e575b0f9e5a853a13
arekiu <alex.isaguilera@hotmail.com>
2024-04-18 16:56:41 +0200

    add isascii

---
+++ ft_isalnum.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschmidt <aschmidt@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 16:45:09 by aschmidt          #+#    #+#             */
+/*   Updated: 2024/04/18 16:45:11 by aschmidt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>
+
+int	ft_isalnum(int numchar);
+
+int	main(void)
+{
+	printf("%d", ft_isalnum('-'));
+}
+*/
+int	ft_isalnum(int num)
+{
+	if ((num >= 'a' && num <= 'z') || (num >= 'A' && num <= 'Z'))
+		return (1);
+	else if (num >= '0' && num <= '9')
+		return (1);
+	else
+		return (0);
+}
---
+++ ft_isascii.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschmidt <aschmidt@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 16:53:52 by aschmidt          #+#    #+#             */
+/*   Updated: 2024/04/18 16:53:54 by aschmidt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>
+
+int	ft_isascii(int numchar);
+
+int	main(void)
+{
+	printf("%d", ft_isascii(128));
+}
+*/
+int	ft_isascii(int num)
+{
+	if (num >= 0 && num <= 127)
+		return (1);
+	else
+		return (0);
+}
