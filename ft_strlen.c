8b15bd96d0f98de4786f1735599c253a78e625ee
arekiu <alex.isaguilera@hotmail.com>
2024-04-18 20:08:00 +0200

    did strlen

---
+++ ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschmidt <aschmidt@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 19:56:38 by aschmidt          #+#    #+#             */
+/*   Updated: 2024/04/18 19:56:41 by aschmidt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+/*#include <stdio.h>
+
+size_t	ft_strlen(const char *str);
+
+int	main(void)
+{
+	printf("%d", (int)ft_strlen("Holischau"));
+}*/
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
