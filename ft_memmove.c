48d6342fc57f88bfbedce206059e0d6cd74924bf
arekiu <alex.isaguilera@hotmail.com>
2024-04-19 11:36:57 +0200

    add memmove

---
+++ ft_memmove.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschmidt <aschmidt@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 09:43:02 by aschmidt          #+#    #+#             */
+/*   Updated: 2024/04/19 09:43:04 by aschmidt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+/*#include <stdio.h>
+#include <string.h>
+
+void	*ft_memmove(void *str1, const void *str2, size_t n);
+
+int main ()
+{
+  char str[] = "memmove can be very useful......";
+  ft_memmove(str+20,str+15,11);
+  puts(str);
+  return 0;
+}*/
+
+void	*ft_memmove(void *str1, const void *str2, size_t n)
+{
+	int	i;
+
+	i = 0;
+	if (str1 == NULL || str2 == NULL)
+		return (NULL);
+	if ((unsigned char *)str1 > (unsigned char *)str2)
+	{
+		while (n > 0)
+		{
+			((unsigned char *)str1)[n - 1] = ((unsigned char *)str2)[n - 1];
+			n--;
+		}
+	}
+	else
+	{
+		while (i < n)
+		{
+			((unsigned char *)str1)[i] = ((unsigned char *)str2)[i];
+			i++;
+		}
+	}
+	return (str1);
+}
