e27ed428a72c400cc13b0df7b834a0521830326c
arekiu <alex.isaguilera@hotmail.com>
2024-04-22 15:44:53 +0200

    did toupper and tolower

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschmidt <aschmidt@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 15:41:57 by aschmidt          #+#    #+#             */
+/*   Updated: 2024/04/22 15:42:23 by aschmidt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>
 
 int	ft_tolower(int ch);
 
@@ -6,7 +18,7 @@ int	main(void)
 {
 	printf("%d", ft_tolower(71));
 }
-
+*/
 int	ft_tolower(int ch)
 {
 	if (ch >= 'A' && ch <= 'Z')
--- ft_toupper.c
+++ ft_toupper.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschmidt <aschmidt@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 15:42:55 by aschmidt          #+#    #+#             */
+/*   Updated: 2024/04/22 15:43:12 by aschmidt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>
 
 int	ft_toupper(int ch);
 
@@ -6,7 +18,7 @@ int	main(void)
 {
 	printf("%d", ft_toupper(101));
 }
-
+*/
 int	ft_toupper(int ch)
 {
 	if (ch >= 'a' && ch <= 'z')
