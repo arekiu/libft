2e1f134b9b4ebe9e52ec57b18a5b84b113c930a9
arekiu <alex.isaguilera@hotmail.com>
2024-04-23 14:43:35 +0200

    add substr

--- adittional/ft_substr.c
+++ adittional/ft_substr.c
@@ -6,11 +6,48 @@
 /*   By: aschmidt <aschmidt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 13:54:47 by aschmidt          #+#    #+#             */
-/*   Updated: 2024/04/23 13:55:27 by aschmidt         ###   ########.fr       */
+/*   Updated: 2024/04/23 14:42:45 by aschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+/*
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
-char *ft_substr(char const *s, unsigned int start, size_t len);
+int	main(void)
+{
+	char *str = "la wea fome";
+
+	printf("FT: %s\n", ft_substr(str, 2, 20));
+}*/
+
+static size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	int		i;
+
+	i = 0;
+	sub = malloc(sizeof(char) * len + 1);
+	if (sub == NULL)
+		return (NULL);
+	while (i < len && s)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
