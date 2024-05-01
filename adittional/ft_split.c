39d25cc63fb9afa00b66614c47d9dee1943d894b
arekiu <alex.isaguilera@hotmail.com>
2024-05-01 13:19:21 +0200

    split finally working

--- adittional/ft_split.c
+++ adittional/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: aschmidt <aschmidt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 10:09:01 by aschmidt          #+#    #+#             */
-/*   Updated: 2024/04/30 10:03:22 by aschmidt         ###   ########.fr       */
+/*   Updated: 2024/05/01 13:18:12 by aschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,158 +16,89 @@
 char	**ft_split(char const *s, char c);
 
 
-static size_t	ft_strlen(const char *str)
+static int	count_wds(char const *s, char c)
 {
-	size_t	i;
+	int	i;
+	int	counter;
 
 	i = 0;
-	while (str[i] != '\0')
+	counter = 0;
+	while (s[i] != '\0')
 	{
-		i++;
+		while (s[i] == c)
+			i++;
+		if (s[i] == '\0')
+			return (counter);
+		counter++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
 	}
-	return (i);
+	return (counter);
 }
 
-static char	*ft_strchr(const char *str, int c)
+static void	*ft_free(char **arr, int elem)
 {
-	int				i;
-	unsigned char	ce;
+	int	i;
 
 	i = 0;
-	ce = c;
-	while (str[i] != '\0')
+	while (i < elem)
 	{
-		if (str[i] == ce)
-			return ((char *)(str + i));
+		free(arr[i]);
 		i++;
 	}
-	if (str[i] == ce)
-		return ((char *)(str + i));
-	return (0);
+	free(arr);
+	return (NULL);
 }
 
-static int      count_wds(char const *s, char c)
+static int	calc_leng(char const *str, int i, char c)
 {
-        int     i;
-        int     counter;
+	int	leng;
 
-        i = 0;
-        counter = 0;
-        while (s[i] != '\0')
-        {
-                while (s[i] == c)
-                        i++;
-                if (s[i] == '\0')
-                        return (counter);
-                counter++;
-                while (s[i] != c && s[i] != '\0')
-                        i++;
-        }
-        return (counter);
+	leng = 0;
+	while (str[i] != c && str[i] != '\0')
+	{
+		leng++;
+		i++;
+	}
+	return (leng);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	**ft_split(char const *str, char c)
 {
-	char	*sub;
+	int		a;
 	int		i;
+	char	**str_arr;
 
+	a = 0;
 	i = 0;
-	sub = malloc(sizeof(char) * len + 1);
-	if (sub == NULL)
+	str_arr = (char **)malloc(sizeof(char *) * (count_wds(str, c) + 1));
+	if (str_arr == NULL)
 		return (NULL);
-	while (i < len && s)
+	while (str[i] != '\0')
 	{
-		sub[i] = s[start + i];
-		i++;
+		while (str[i] == c)
+			i++;
+		if (str[i] != '\0')
+		{
+			str_arr[a] = ft_substr(str, i, calc_leng(str, i, c));
+			if (str_arr[a] == NULL)
+				return (ft_free(str_arr, a));
+			a++;
+			i = i + calc_leng(str, i, c);
+		}
 	}
-	sub[i] = '\0';
-	return (sub);
-}
-
-
-static void *ft_free(char **arr, int elem)
-{
-    int i;
-
-    i = 0;
-    while (i < elem)
-    {
-        free(arr[i]);
-        i++;
-    }
-    free(arr);
-    return (NULL);
-}
-
-static char **put_word(char **arr, char const *str, int words, char c)
-{
-        unsigned int    i;
-        int             a;
-        int             start;
-
-        i = 0;
-        a = 0;
-        while (str[i] != '\0' && a < words)
-        {
-                while (str[i] == c)
-                        i++;
-                if (str[i] != '\0')
-                {
-                        start = i;
-                        while (str[i] != c && str[i] != '\0')
-                                i++;
-                        arr[a] = ft_substr(str, start, i - start);
-                        if (!(arr[a]))
-                        {
-                                ft_free(arr, a);
-                                return NULL;
-                        }
-                        a++;
-                        while (str[i] == c)
-                                i++;
-                }
-        }
-        arr[a] = NULL;
-        return arr;
-}
-
-char    **ft_split(char const *str, char c)
-{
-        int             words;
-        char    **str_arr;
-
-        if (str[0] == '\0')
-                words = 0;
-        else
-        {
-                words = count_wds(str, c);
-                if (words == 0)
-                        words = 1;
-        }
-        str_arr = (char **)malloc(sizeof(char *) * (words + 1));
-        if (str_arr == NULL)
-                return (NULL);
-        if (ft_strchr(str, c) == 0 && str[0] != '\0')
-        {
-                str_arr[0] = ft_substr(str, 0, ft_strlen(str));
-                if (!(str_arr[0]))
-                        ft_free(str_arr, 0);
-                str_arr[1] = NULL;
-        }
-        else
-                put_word(str_arr, str, words, c);
-        return (str_arr);
+	str_arr[a] = '\0';
+	return (str_arr);
 }
 /*
-int	main(void)
+int     main(void)
 {
-	char *str = " holische";
-	char	c = ' ';
+        char *str = "  Esta wea qlia fome del ort ";
+        char    c = ' ';
 
-	int num_words = count_wds(str,c);
-	printf("num wds %d\n", num_words);
-	char **array = ft_split(str, c);
-	printf("la palabra %s\n", array[1]);
+        int num_words = count_wds(str,c);
+        printf("num wds %d\n", num_words);
 
-	return (0);
-}*/
+        return (0);
+}
+*/
--- libc/ft_memmove.c
+++ libc/ft_memmove.c
@@ -3,25 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aschmidt <aschmidt@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: aschmidt <aschmidt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 09:43:02 by aschmidt          #+#    #+#             */
-/*   Updated: 2024/04/24 13:57:32 by aschmidt         ###   ########.fr       */
+/*   Updated: 2024/05/01 13:18:48 by aschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+//#include "libft.h"
 #include <unistd.h>
-/*#include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 
 void	*ft_memmove(void *str1, const void *str2, size_t n);
-
+/*
 int main ()
 {
   char str[] = "memmove can be very useful......";
   ft_memmove(str+20,str+15,11);
   puts(str);
+	char b[0xF0];
+
+ 	ft_memmove(((void*)0), b, 5);
+
+
   return 0;
 }*/
 
